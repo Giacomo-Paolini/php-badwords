d5ba5a9482ef60b09829f94cae106724e12540bb
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-06-07 16:26:52 +0200

    fix layout

--- badword.php
+++ badword.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>PHP Badwords</title>
 </head>
 <body>
@@ -14,12 +15,16 @@
         $textFixed = str_ireplace($badword, "***", $text,);
     ?>
     <main>
-        <h4>TEXT AREA</h4>
-        <p><?php echo $text; ?></p>
-        <p>Caratteri: <?php echo strlen( $text )?></p>
-        <br>
-        <h4>BADWORD</h4>
-        <p><?php echo $textFixed; ?></p>
+        <div class="container">
+            <div class="row text-center p-4">
+                <h4>TEXT AREA</h4>
+                <p><?php echo $text; ?></p>
+                <small>Caratteri: <?php echo strlen( $text )?></small>
+                <br>
+                <h4 class="mt-4">BADWORD</h4>
+                <p><?php echo $textFixed; ?></p>
+            </div>
+        </div>
     </main>
 </body>
 </html>
--- form.php
+++ form.php
@@ -4,13 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>PHP Badwords</title>
 </head>
 <body>
-    <form action="badword.php" method="GET">
-        <textarea placeholder="Text area" name="textArea"></textarea>
-        <input type="text" name="badword" placeholder="Inserisci una parola che si trova nella text area">
-        <button type="submit">Send</button>
-    </form>
+    <div class="container">
+        <div class="row">
+            <form action="badword.php" method="GET" class="p-4">
+                <div class="col-12 gap-4">
+                    <div class="d-flex flex-column">
+                        <h4>TEXT AREA</h4>
+                        <textarea placeholder="Text area" name="textArea"></textarea>
+                    </div>
+                    <div class="d-flex flex-column mt-4">
+                        <h4>BADWORD</h4>
+                        <input type="text" name="badword" placeholder="Inserisci una parola che si trova nella text area">
+                    </div>
+                </div>
+                <div class="col-4 offset-4 text-center mt-2">
+                    <button class="col-2" type="submit">Send</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
